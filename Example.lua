8d4a4947af302d5eb458d60628b3a08a5fcfdbe1
softbf395 <130502864+softbf395@users.noreply.github.com>
2025-01-13 15:00:29 -0600

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,4 @@
+local utility=loadstring(game:HttpGet("https://raw.githubusercontent.com/softbf395/Sync-script/refs/heads/main/Main.lua"))()
+utility.share([[--example code
+  game.Players.LocalPlayer.Character.Head.BrickColor=BrickColor.random()
+  ]])
